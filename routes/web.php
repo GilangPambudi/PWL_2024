7dbea9a0eb4886f7c7b30aa5410528f3b258b04c
GilangPambudi <2131710126@student.polinema.ac.id>
2024-02-22 14:32:22 +0700

    Penambahan /user/{name?}

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,6 @@ Route::get('/about', function () {
     return '2141762126 - Gilang Pambudi Wibawanto'; 
 });
 
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya '.$name;
-});
-
 Route::get('/posts/{post}/comments/{comment}', function
 ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
@@ -45,4 +41,8 @@ Route::get('/posts/{post}/comments/{comment}', function
 Route::get('/articles/{id}', function
 ($id) {
     return 'Halaman artikel dengan ID: '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
