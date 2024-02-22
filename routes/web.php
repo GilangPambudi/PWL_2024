21404f91fa402e8b40fb6bc1f5e29881d083924c
GilangPambudi <2131710126@student.polinema.ac.id>
2024-02-22 15:55:36 +0700

    Update pada greeting dan WelcomeController

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,5 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('/greeting', function () {
-    return view('blog.hello', ['name' => 'Andi']);
-});
+Route::get('/greeting', [WelcomeController::class,
+'greeting']); 
